9827654c1e0a81ad8b86aa0196fb7113eee157e3
tonchan1216 <cleartone1216@gmail.com>
2018-09-06 18:09:27 +0900

    delete confirm

--- src/App.js
+++ src/App.js
@@ -2,17 +2,162 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const todos = [];
+
+function TodoHeader(props) {
+  const remaining = props.todos.filter(todo => {
+    return !todo.isDone;
+  });
+
+  return (
+    <h1>
+      <button onClick={props.purge}>Purge</button>
+      My Todos
+      <span>({remaining.length}/{props.todos.length})</span>
+    </h1>
+  );
+}
+
+function TodoItem(props) {
+  return (
+    <li key={props.todo.id}>
+      <label>
+        <input type="checkbox"
+          checked={props.todo.isDone}
+          onChange={() => props.checkTodo(props.todo)}
+        />
+        <span className={props.todo.isDone ? 'done' : ''}>
+          {props.todo.title}
+        </span>
+      </label>
+      <span className="cmd" onClick={() => props.deleteTodo(props.todo)}>[x]</span>
+    </li>
+  );
+}
+
+function TodoList(props) {
+  const todos = props.todos.map(todo => {
+    return (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        checkTodo={props.checkTodo}
+        deleteTodo={props.deleteTodo}
+      />
+    );
+  });
+  return (
+    <ul>
+      {props.todos.length ? todos : <li>Nothing to do!</li>}
+    </ul>
+  );
+}
+
+function TodoForm(props) {
+  return (
+    <form onSubmit={props.addTodo}>
+      <input type="text" value={props.item} onChange={props.updateItem}/>
+      <input type="submit" value="Add"/>
+    </form>
+  );
+}
+
+function getUniqueId() {
+  return new Date().getTime().toString(36) + '-' + Math.random().toString(36);
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todos,
+      item: ''
+    };
+    this.checkTodo = this.checkTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.updateItem = this.updateItem.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.purge = this.purge.bind(this);
+  }
+
+  purge() {
+    const todos = this.state.todos.filter(todo => {
+      return !todo.isDone;
+    });
+    this.setState({
+      todos: todos
+    });
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+
+    if (this.state.item.trim() === '') {
+      return;
+    }
+
+    const item = {
+      id: getUniqueId(),
+      title: this.state.item,
+      isDone: false
+    };
+
+    const todos = this.state.todos.slice();
+    todos.push(item);
+    this.setState({
+      todos: todos,
+      item: ''
+    });
+  }
+
+  deleteTodo(todo) {
+    const todos = this.state.todos.slice();
+    const pos = this.state.todos.indexOf(todo);
+
+    todos.splice(pos, 1);
+    this.setState({
+      todos: todos
+    });
+  }
+
+  checkTodo(todo) {
+    const todos = this.state.todos.map(todo => {
+      return {id: todo.id, title: todo.title, isDone: todo.isDone};
+    });
+
+    const pos = this.state.todos.map(todo => {
+      return todo.id;
+    }).indexOf(todo.id);
+
+    todos[pos].isDone = !todos[pos].isDone;
+    this.setState({
+      todos: todos
+    });
+  }
+
+  updateItem(e) {
+    this.setState({
+      item: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <TodoHeader
+          todos={this.state.todos}
+          purge={this.purge}
+        />
+        <TodoList
+          todos={this.state.todos}
+          checkTodo={this.checkTodo}
+          deleteTodo={this.deleteTodo}
+        />
+        <TodoForm
+          item={this.state.item}
+          updateItem={this.updateItem}
+          addTodo={this.addTodo}
+        />
       </div>
     );
   }
